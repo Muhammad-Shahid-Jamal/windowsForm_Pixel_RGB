120da8c11f70acb27e16b7c0d8826963721f9af0
Shahid <shahid@braincrop.net>
2018-12-16 22:06:36 +0500

    form complete getting image rgb value on mouse move on image

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rgb_text = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.upload_pic = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(653, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RGB Value";
+            // 
+            // rgb_text
+            // 
+            this.rgb_text.Location = new System.Drawing.Point(656, 29);
+            this.rgb_text.Name = "rgb_text";
+            this.rgb_text.Size = new System.Drawing.Size(262, 22);
+            this.rgb_text.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(906, 525);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MouseMvEv);
+            // 
+            // upload_pic
+            // 
+            this.upload_pic.Location = new System.Drawing.Point(12, 28);
+            this.upload_pic.Name = "upload_pic";
+            this.upload_pic.Size = new System.Drawing.Size(108, 23);
+            this.upload_pic.TabIndex = 3;
+            this.upload_pic.Text = "Select Pic";
+            this.upload_pic.UseVisualStyleBackColor = true;
+            this.upload_pic.Click += new System.EventHandler(this.upload_pic_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(930, 600);
+            this.Controls.Add(this.upload_pic);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.rgb_text);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Picture RGB value";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox rgb_text;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button upload_pic;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,32 @@ namespace windowsForm_Pixel_RGB
 {
     public partial class Form1 : Form
     {
+        private Bitmap bmp;
         public Form1()
         {
             InitializeComponent();
+            bmp = null;
+        }
+
+        private void upload_pic_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            pictureBox1.Image = new Bitmap(openFileDialog1.FileName);
+            bmp = new Bitmap(pictureBox1.Image,new Size(pictureBox1.Size.Width,pictureBox1.Size.Height));
+
+        }
+
+        private void MouseMvEv(object sender, MouseEventArgs e)
+        {
+            if(bmp != null)
+            {
+                if(e.Location.X < bmp.Width && e.Location.Y < bmp.Height)
+                {
+                    Color rgb = bmp.GetPixel(e.Location.X, e.Location.Y);
+                    string rgbVlaue = "R: " + rgb.R.ToString() + " G: " + rgb.G.ToString() + " B: " + rgb.B.ToString();
+                    rgb_text.Text = rgbVlaue;
+                }
+            }
         }
     }
 }
